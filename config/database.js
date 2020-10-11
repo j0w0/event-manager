a5b0194338d51ddc017bab753a8955b19401ff78
Josh Woodcock <josh@j0w0.com>
2020-10-11 18:56:51 -0500

    Fix port on database connection

--- config/database.js
+++ config/database.js
@@ -9,5 +9,5 @@ mongoose.connect(process.env.DATABASE_URL, {
 const db = mongoose.connection;
 
 db.on('connected', function() {
-    console.log(`Mongoose connected to: ${db.host}:${port}`);
+    console.log(`Mongoose connected to: ${db.host}:${db.port}`);
 });
